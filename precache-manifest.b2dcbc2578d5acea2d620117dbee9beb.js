d34f58ce772e2e25c56855d5769dae4f6586f42b
Yusuf Mert Çakmak <ymert60@hotmail.com>
2019-10-08 17:59:46 +0300

    Updates

---
+++ precache-manifest.b2dcbc2578d5acea2d620117dbee9beb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97f0acf4724f5f651595a4c9534cb83e",
+    "url": "/periodic-table/index.html"
+  },
+  {
+    "revision": "96f6462ab69e3ee1fa6c",
+    "url": "/periodic-table/static/css/main.0b719ac3.chunk.css"
+  },
+  {
+    "revision": "cfd5246edce99dd641da",
+    "url": "/periodic-table/static/js/2.430b9de1.chunk.js"
+  },
+  {
+    "revision": "96f6462ab69e3ee1fa6c",
+    "url": "/periodic-table/static/js/main.9f4d35b4.chunk.js"
+  },
+  {
+    "revision": "1b471a4abff3187f9ef2",
+    "url": "/periodic-table/static/js/runtime-main.0eebffd5.js"
+  }
+]);
